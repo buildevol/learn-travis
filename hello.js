2e45319812bcb0f03f1728a745f361494643915e
JonathanLeeWH <leeweihao1996@outlook.com>
2020-05-23 17:32:34 +0800

    Add Travis support

---
+++ hello.js
@@ -0,0 +1,13 @@
+/*jshint esversion: 6 */
+
+const http = require("http");
+const PORT = proess.env.PORT;
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello Travis!\n");
+  })
+  .listen(PORT, "127.0.0.1");
+
+console.log(`Server running at http://127.0.0.1:${PORT}`);
